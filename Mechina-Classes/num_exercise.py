762efacc712d195562bd9199dc794326fa2b7470
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-05-26 13:13:57 +0300

    another exercise

---
+++ Mechina-Classes/num_exercise.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number with 5 digits:\n"))
+
+ten_thousand = int(num / 10000)
+thousand = int(num % 10000 / 1000)
+hundreds = int(num % 1000 / 100)
+dozens = int(num % 100 / 10)
+units = int(num % 10)
+sum_digits = ten_thousand + thousand + hundreds + dozens + units
+print(f"You enter the number {num}")
+print(f"The digits of the number are: {ten_thousand},{thousand},{hundreds},{dozens},{units}")
+print(f"The sum of the digits is: {sum_digits}")
