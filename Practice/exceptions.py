4ab8ff103890bf2e6352a3814dd6b2c40796947f
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-06-14 17:23:20 +0300

    trying exceptions

--- Practice/exceptions.py
+++ Practice/exceptions.py
@@ -1,9 +1,9 @@
-# while True:
-#     try:
-#         x = int(input("Please enter a number: "))
-#         break
-#     except ValueError:
-#         print("Oops!  That was no valid number.  Try again...")
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
 
 x = int(input("Enter a number:"))
 y = int(input("Enter divider number:"))
@@ -12,3 +12,11 @@ try:
     print(divide)
 except:
     print("You cant divide by zero")
+
+sentence = input("Enter some words:")
+anything = int(input("Enter a number:"))
+try:
+    sentence += anything
+except:
+    print("Just kiddin' this is an exception to write the way i did :)")
+    print("Python can not add int to string with '+=' you can use '{}' or '' :)")
