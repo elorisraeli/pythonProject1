02723a6de1fce2506613a416c5ca23f99469e330
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-06-14 17:13:56 +0300

    trying exceptions

--- Practice/exceptions.py
+++ Practice/exceptions.py
@@ -1,6 +1,14 @@
-while True:
-    try:
-        x = int(input("Please enter a number: "))
-        break
-    except ValueError:
-        print("Oops!  That was no valid number.  Try again...")
+# while True:
+#     try:
+#         x = int(input("Please enter a number: "))
+#         break
+#     except ValueError:
+#         print("Oops!  That was no valid number.  Try again...")
+
+x = int(input("Enter a number:"))
+y = int(input("Enter divider number:"))
+try:
+    divide = x / y
+    print(divide)
+except:
+    print("You cant divide by zero")
