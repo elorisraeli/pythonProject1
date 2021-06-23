a7503dbd4ff5fa2311b0a5502c781be771b16885
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-06-23 18:56:59 +0300

    more exercises

--- Mechina-Classes/list_exercises.py
+++ Mechina-Classes/list_exercises.py
@@ -27,15 +27,60 @@
 # print(summer(['aa', 'bb', 'cc', 'dd']))
 # print(summer([1, 2, 3, 'a'], [4, 'b', 'c', 'd']))
 
-# Exercise 3: ---Sort list by last char of the elements---
-animals = ['cat', 'zebra', 'dog', 'giraffe']
+# # Exercise 3: ---Sort list by last char of the elements---
+# animals = ['cat', 'zebra', 'dog', 'giraffe']
+#
+#
+# def last(lst1):
+#     return lst1[::-1]
+#
+#
+# lst = sorted(animals, key=last)
+# print(lst)
 
 
-def last(lst1):
-    return lst1[::-1]
+# # # After tuple explanation
+# # Exercise 1: ---Create List with jumps of 2---
+# lst = []
+# for i in range(2, 11):
+#     if i % 2 == 0:
+#         lst.append(i)
+# print(lst)
 
+# # Exercise 2: ---Delete empty elements from list---
+# lst = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
+# for i in lst:
+#     if type(i) != str or i == "":
+#         lst.remove(i)
+# print(lst)
 
-lst = sorted(animals, key=last)
-print(lst)
+# # Exercise 3: ---Get list from user---
+# string = input("Enter sentence:")
+# string = string.split(" ")
+# print(string)
 
+# # Exercise 4: ---List of chars and their count in string---
+# line = input("Enter a line:")
+# lst = []
+# for char in line:
+#     if char in lst:
+#         pass
+#     else:
+#         lst.append(char)
+#         lst.append(line.count(char))
+# print(lst)
 
+# Exercise 5: ---Under 18 only first name, else last name too---
+import datetime
+
+year_now = datetime.datetime.now().year
+year_born = int(input("Enter the year you were born:"))
+# ^^^ also could just ask for age :) ^^^
+# like that -> age = input("Enter your age:")
+first = input("Enter your first name:")
+age = year_now - year_born
+if age < 18:
+    print(f"Thank you {first} and goodbye")
+else:
+    last = input("Enter your last name:")
+    print(f"Thank you for using {first} {last}, have a great day")
