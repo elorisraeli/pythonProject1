58c424392bcefa42b355cc39d2a3d27d91c39c26
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-06-29 17:45:10 +0300

    dictionary first mission

---
+++ Mechina-Classes/dictionary.py
@@ -0,0 +1,22 @@
+# Exercise 1: ---Shopping exercise---
+prices = {
+    'bananas': 10,
+    'apples': 8,
+    'bread': 7,
+    'cheese': 20,
+    'juice': 15
+}
+shopping_cart = {
+    'bananas': 2,
+    'bread': 1,
+    'cheese': 3
+}
+print(shopping_cart.values())
+print(prices.values())
+
+total = 0
+for s in shopping_cart.keys():
+    for p in prices.keys():
+        if s == p:
+            total += shopping_cart[s] * prices[p]
+print(f"The total of your shopping is: {total}")
