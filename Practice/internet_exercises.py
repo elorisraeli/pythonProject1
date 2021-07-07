7b7e73c2ede82f58af2c8f192e67e32fa99a9839
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-07-07 23:40:35 +0300

    easy way added

--- Practice/internet_exercises.py
+++ Practice/internet_exercises.py
@@ -1,16 +1,25 @@
-# Exercise Palindrome: ---Check if word is palindrome (write back the same like usual)---
-word = input("Enter a word for check:").lower()
-isPal = False
-i = 0
-while i < len(word)/2:
-    print(word[i], word[len(word)-i-1])
-    if word[i] != word[len(word)-i-1]:
-        isPal = False
-        break
-    else:
-        i += 1
-        isPal = True
-if isPal:
-    print(f"The word {word} is Palindrome.")
+# # Exercise Palindrome: ---Check if word is palindrome (write back the same like usual)---
+# word = input("Enter a word for check:").lower()
+# isPal = False
+# i = 0
+# while i < len(word)/2:
+#     print(word[i], word[len(word)-i-1])
+#     if word[i] != word[len(word)-i-1]:
+#         isPal = False
+#         break
+#     else:
+#         i += 1
+#         isPal = True
+# if isPal:
+#     print(f"The word {word} is Palindrome.")
+# else:
+#     print(f"The word {word} is NOT Palindrome.")
+
+# OR THE EASY WAY :)
+text = input("enter a word:").lower()
+
+if text == text[::-1]:
+    print(text, text[::-1])
+    print(text, "is a palindrome")
 else:
-    print(f"The word {word} is NOT Palindrome.")
+    print(text, "is not a palindrome")
