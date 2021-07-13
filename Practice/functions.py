721e055d06914870e4d4df46d82ece9a15ff2742
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-07-13 16:07:51 +0300

    simple functions

--- Practice/functions.py
+++ Practice/functions.py
@@ -1,6 +1,6 @@
 # Functions which doing basic things:
 
-# Example 1:
+# Example 1: Reverse string
 def end_to_start(string):
     return string[::-1]
 
@@ -8,7 +8,7 @@ def end_to_start(string):
 print(end_to_start("sdfkjghsdjkfgheoir"))
 
 
-# Example 2:
+# Example 2: Calculator
 def math_calc(num1, operator, num2):
     if num1 == int(num1) and num2 == int(num2):
         if operator == '+':
@@ -26,3 +26,16 @@ def math_calc(num1, operator, num2):
 
 
 print(math_calc(4, "*", 5))
+
+
+# Example 3: Sum of list
+def sum_list(lst):
+    total = 0
+    for i in lst:
+        # check if the index in the list is an integer number
+        if i == int(i):
+            total += i
+    return total
+
+
+print(sum_list([1, 3, 100, 7]))
