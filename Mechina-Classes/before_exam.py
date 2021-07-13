f8b0a15e5bc3f5b1f4c12b1e9d4598475fd94d7b
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-07-13 18:19:34 +0300

    extra

---
+++ Mechina-Classes/before_exam.py
@@ -0,0 +1,50 @@
+# # Exercise 1: ---Run until 'q', sum all numbers---
+# total = 0
+# while True:
+#     num = input("Enter numbers, if you done enter 'q':")
+#     if num.isdigit():
+#         int_num = int(num)
+#         total += int_num
+#     elif num == "q":
+#         print(f"Sum is: {total}")
+#         break
+
+
+# # Exercise 2: ---Get list, number. clean list, don't have number dividing by number---
+# def clean_list(lst, divisor):
+#     for i in lst:
+#         if i % divisor == 0:
+#             lst.remove(i)
+#     return lst
+
+
+# print(clean_list([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
+
+# # Exercise 3: ---Check if string is same when reversed---
+# string = input("Enter something to check:")
+# if string == string[::-1]:
+#     print(f"The string '{string}' is palindrome")
+# else:
+#     print("NOT a palindrome")
+
+
+# Exercise 4: ---Lotto game, guess 3 different numbers between 0-9 to win---
+import random
+guess1 = int(input("Guess number 1:"))
+guess2 = int(input("Guess another number:"))
+guess3 = int(input("Guess another number:"))
+win_numbers = []
+# get the random numbers into the list
+while True:
+    number = random.randint(0, 9)
+    if number not in win_numbers:
+        win_numbers.append(number)
+    else:
+        pass
+    if len(win_numbers) == 3:
+        break
+# check if guesses in the list of random numbers
+if guess1 in win_numbers and guess2 in win_numbers and guess3 in win_numbers:
+    print(f"You Win \n Lotto winner numbers are: {win_numbers}")
+else:
+    print(f"You Lose \n Lotto winner numbers are: {win_numbers}")
