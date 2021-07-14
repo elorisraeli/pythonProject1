be30ee2ba145b1da5ceb8e7580aeb3ff010a9806
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-07-14 22:03:22 +0300

    more practice

--- Mechina-Classes/before_exam.py
+++ Mechina-Classes/before_exam.py
@@ -28,23 +28,36 @@
 #     print("NOT a palindrome")
 
 
-# Exercise 4: ---Lotto game, guess 3 different numbers between 0-9 to win---
-import random
-guess1 = int(input("Guess number 1:"))
-guess2 = int(input("Guess another number:"))
-guess3 = int(input("Guess another number:"))
-win_numbers = []
-# get the random numbers into the list
-while True:
-    number = random.randint(0, 9)
-    if number not in win_numbers:
-        win_numbers.append(number)
-    else:
-        pass
-    if len(win_numbers) == 3:
-        break
-# check if guesses in the list of random numbers
-if guess1 in win_numbers and guess2 in win_numbers and guess3 in win_numbers:
-    print(f"You Win \n Lotto winner numbers are: {win_numbers}")
-else:
-    print(f"You Lose \n Lotto winner numbers are: {win_numbers}")
+# # Exercise 4: ---Lotto game, guess 3 different numbers between 0-9 to win---
+# import random
+# guess1 = int(input("Guess number 1:"))
+# guess2 = int(input("Guess another number:"))
+# guess3 = int(input("Guess another number:"))
+# win_numbers = []
+# # get the random numbers into the list
+# while True:
+#     number = random.randint(0, 9)
+#     if number not in win_numbers:
+#         win_numbers.append(number)
+#     else:
+#         pass
+#     if len(win_numbers) == 3:
+#         break
+# # check if guesses in the list of random numbers
+# if guess1 in win_numbers and guess2 in win_numbers and guess3 in win_numbers:
+#     print(f"You Win \n Lotto winner numbers are: {win_numbers}")
+# else:
+#     print(f"You Lose \n Lotto winner numbers are: {win_numbers}")
+
+
+# Exercise 5: ---Sum numbers from txt file---
+total = 0
+with open(r'C:\Users\Elor Israeli\Desktop\מבוא למחשבים\homework/numbers.txt', 'r') as file_nums:
+    for line in file_nums:
+        nums_list = line.split(" ")
+        for num in nums_list:
+            if num.isdigit():
+                total += int(num)
+        nums_list.clear()
+print(f"The sum of all the numbers in the file is: {total}")
+
