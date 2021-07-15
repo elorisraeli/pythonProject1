a34bbcc1a5312f98b27a45807e5c445c9fa39e6b
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-07-15 19:13:22 +0300

    explore

--- Practice/list_practice.py
+++ Practice/list_practice.py
@@ -1,7 +1,22 @@
-# Before Exam Mechina: ---Sort a list---
-lst = ["ab", "cde", "fghi", "jklmn"]
-print(f"List: {lst}")
-lst.sort(reverse=True)
-print(f"List reverse: {lst}")
+# # Before Exam Mechina: ---Sort a list---
+# lst = ["ab", "cde", "fghi", "jklmn"]
+# print(f"List: {lst}")
+# lst.sort(reverse=True)
+# print(f"List reverse: {lst}")
+#
+#
+# # sort by key, this time is length of string in list
+# def sort_len(e):
+#     return len(e)
+#
+#
+# lst2 = ["cde", "ab", "jklmn", "fghi", ]
+# lst2.sort(key=sort_len)
+# print(lst2)
 
+# sort by key, this time is first date in list
+from datetime import datetime
 
+values = ['8-Nov-19', '21-Jun-16', '1-Nov-18', '7-Apr-19']
+values.sort(key=lambda d: datetime.strptime(d, "%d-%b-%y"))
+print(values)
