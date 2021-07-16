660076c7fd7f8b1a427edd2f56ac47b2d703bcd0
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-07-16 14:27:28 +0300

    list practice

--- Practice/list_practice.py
+++ Practice/list_practice.py
@@ -14,9 +14,29 @@
 # lst2.sort(key=sort_len)
 # print(lst2)
 
-# sort by key, this time is first date in list
-from datetime import datetime
+# # sort by key, this time is first date in list
+# from datetime import datetime
+#
+# values = ['8-Nov-19', '21-Jun-16', '1-Nov-18', '7-Apr-19']
+# values.sort(key=lambda d: datetime.strptime(d, "%d-%b-%y"))
+# print(values)
 
-values = ['8-Nov-19', '21-Jun-16', '1-Nov-18', '7-Apr-19']
-values.sort(key=lambda d: datetime.strptime(d, "%d-%b-%y"))
-print(values)
+
+# function return how much of each type is in the list
+def type_count(lst):
+    new_lst = [["int", 0], ["float", 0], ["string", 0], ["boolean", 0]]
+    if isinstance(lst, list):
+        for element in lst:
+            if type(element) == int:
+                new_lst[0][1] += 1
+            elif type(element) == float:
+                new_lst[1][1] += 1
+            elif type(element) == str:
+                new_lst[2][1] += 1
+            elif type(element) == bool:
+                new_lst[3][1] += 1
+    return new_lst
+
+
+combined_list = [1, "ertgb", 34.343, True, 54, "ewptbw", False, 21.98]
+print(type_count(combined_list))
