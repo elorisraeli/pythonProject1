dd8629823d381fcd8a0b0541bf1132036252b223
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-07-18 19:54:43 +0300

    files practice

---
+++ Practice/letters/letters_files.py
@@ -0,0 +1,8 @@
+# Create files in folder, by alphabet
+import string, os
+
+if not os.path.exists("letters"):
+    os.makedirs("letters")
+for letter in string.ascii_uppercase:
+    with open(letter + ".txt", "w") as f:
+        f.writelines(letter)
